daef55420df2cd8b0269ff7ca774c951a109f2b3
Fikri Zufri <fikrizufri@gmail.com>
2023-10-03 23:19:32 +0800

    :hammer: aduh

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Subject extends Model
 {
     use HasFactory;
+    public function subject()
+    {
+        return $this->belongsTo(Subject::class);
+    }
+
+    public function student()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
